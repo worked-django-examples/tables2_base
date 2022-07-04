1632a7f79831365c031afd6a84e4e4940d9e81e6
gilberto <levin@readytheory.com>
2022-07-04 12:41:40 -0500

    add country data and script to populate two tables

--- django/countries/models.py
+++ django/countries/models.py
@@ -1,11 +1,21 @@
 from django.db import models
+from django.db.models.constraints import UniqueConstraint
 
 # Create your models here.
 
+
 class Country(models.Model):
     country_code = models.CharField(max_length=5, primary_key=True)
-    name = models.TextField(null=False) 
+    name = models.TextField(null=False)
+
+
 class AnnualPopulation(models.Model):
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    year = models.IntegerField(null=True)
+
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, name="country")
+    year = models.IntegerField(null=False)
     population = models.IntegerField(null=True)
+    year_country = models.CharField(max_length=9, unique=True)
+    # year_country is for uniqueness since foreign
+    # key can't be part of composite unique
+
+    
