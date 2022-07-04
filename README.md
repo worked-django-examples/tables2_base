e3e5c9b999965168ab2d07dc9ba3ae22b792bc4b
gilberto <levin@readytheory.com>
2022-07-04 12:41:40 -0500

    add country data and script to populate two tables

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
-[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/worked-django-examples/tables2_base)
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/worked-django-examples/tables2_base/tree/country-wip)
 
 # Demos, walkthrus, code inspection django-tables2
 
-# from github.com/worked-django-examples 
+from https://github.com/worked-django-examples 
 
 ## Purpose
 
 This is a base for creating examples of django-tables2 examples on gitpod.
-The gitpod image will load django and postgres.
+The gitpod image will load django and postgres, and a couple tables with
+country population data.
 
-## setup 
+### repo setup 
 
  - `pip install django_tables2 tablib bootstrap4 psycopg2-binary python-env`**** 
     (or `pip install -r requirementsl.txt`)
@@ -18,8 +19,11 @@ The gitpod image will load django and postgres.
  - create `.gitignore`
  - add `.env` to `.gitignore`
  - change settings for passwords/CSRF secrets
+ - add files from worldbank to "etl/worldbank"
  ## 
 
 ## to run on your local with docker
 
 `docker run --name postgresql -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 --restart always -v /tmp/pgdata:/tmp/var/lib/postgresql/data -d postgres:13`
+
+run the worldbank-population.py script in the etl directory
--- etl/worldbank-population.py
+++ etl/worldbank-population.py
@@ -1,5 +1,7 @@
 import csv
 import os
+from collections import defaultdict
+from psycopg2.extras import execute_batch
 
 import psycopg2 as pg
 import configparser
@@ -14,7 +16,24 @@ if os.getenv("GITPOD_WORKSPACE_CONTEXT", False):
 else:
     config_file = DEFAULT_CONFIG_FILE
 
-# Series Name,"Series Code","Country Name","Country Code","2000 [YR2000]","2001 [YR2001]","2002 [YR2002]","2003 [YR2003]","2004 [YR2004]","2005 [YR2005]","2006 [YR2006]","2007 [YR2007]","2008 [YR2008]","2009 [YR2009]","2010 [YR2010]","2011 [YR2011]","2012 [YR2012]","2013 [YR2013]","2014 [YR2014]","2015 [YR2015]"
+configp = configparser.ConfigParser()
+configp.read(config_file)
+postgres = configp["postgres"]
+port, password, user, database, host = (
+    postgres["port"],
+    postgres["password"],
+    postgres["user"],
+    postgres["database"],
+    postgres["url"],
+)
+
+
+# connect to the database before bothering to read the data
+dbconn = pg.connect(dbname=database, user=user, password=password, host=host, port=port)
+dbcursor = dbconn.cursor()
+
+
+# Series Name',"Series Code","Country Name","Country Code","2000 [YR2000]","2001 [YR2001]","2002 [YR2002]","2003 [YR2003]","2004 [YR2004]","2005 [YR2005]","2006 [YR2006]","2007 [YR2007]","2008 [YR2008]","2009 [YR2009]","2010 [YR2010]","2011 [YR2011]","2012 [YR2012]","2013 [YR2013]","2014 [YR2014]","2015 [YR2015]"
 
 YEAR_COLUMNS = [
     "2000 [YR2000]",
@@ -35,24 +54,53 @@ YEAR_COLUMNS = [
     "2015 [YR2015]",
 ]
 
-inserts = []
+population_inserts = []
+
+country_inserts = []
+
 with open(DATAFILE, "rt", encoding="utf-8-sig") as data:
     reader = csv.DictReader(data, quotechar='"')
     for i, row in enumerate(reader):
         if row["Series Name"] == "Population, total":
-            country = row['Country Code']
+            country = row["Country Code"]
+            country_inserts.append(
+                {"country_code": country, "name": row["Country Name"]}
+            )
             for year_column in YEAR_COLUMNS:
                 try:
                     population = int(row[year_column])
-                except Exception  as e:
+                except Exception as e:
                     print(f"no population for {country} for {year_column}")
                     print(e)
                     population = None
                 year = int(year_column[:4])
-                inserts.append({"country": country,
-                                "year": year,
-                                "population": population})
-                                
-    print(f"Length of inserts: {len(inserts)}")
-    
-                                
+                population_inserts.append(
+                    {
+                        "year": year,
+                        "population": population,
+                        "country_id": country,
+                        "year_country": f"{year}{country}",
+                    }
+                )
+
+    COUNTRY_SQL = """insert into countries_country
+                  VALUES
+                  (%(country_code)s, %(name)s)
+                  ON CONFLICT(country_code) DO NOTHING"""
+    execute_batch(dbcursor, sql=COUNTRY_SQL, argslist=country_inserts)
+    dbconn.commit()
+
+    dbcursor = dbconn.cursor()
+
+    ANNUAL_POP_SQL = """insert into countries_annualpopulation\
+            (country_id, year, population, year_country)
+            VALUES
+            (%(country_id)s,  %(year)s, %(population)s, %(year_country)s)
+            ON CONFLICT(year_country) DO UPDATE
+            SET population = %(population)s
+"""
+    execute_batch(dbcursor, sql=ANNUAL_POP_SQL, argslist=population_inserts)
+
+    dbconn.commit()
+
+    print("done")
