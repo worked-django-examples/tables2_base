41bc336ca44f1404228af72d75c4257111377f9b
gilberto <levin@readytheory.com>
2022-07-04 15:52:51 -0500

    Add sample data, and page to show data is live

--- django/countries/views.py
+++ django/countries/views.py
@@ -14,16 +14,33 @@ def index(request):
         year = something.year
         country = something.country.name
         population = something.population
-        return(year, country, population)
+        return (year, country, population)
 
     the_facts = ""
     for offset in [5, 25, 15]:
         year, country, population = facts(offset)
-        the_facts = (f"{the_facts}<P>"
-                     f"the population {country} was {population} "
-                     f"in {year}")
+        the_facts = (
+            f"{the_facts}<P>"
+            f"the population of {country} was {population} "
+            f"in {year}"
+        )
 
+    so_what = """<P><b>If you are looking at this gitpod</b>, and see
+                 population numbers about countries above, it means Django is running
+                 and pulling data from a postrgres database.  
+
+                 <P>you can start a new shell and run <code>python manage.py shell</code> or other django stuff like
+                 <code>startapp</app>.
+
+                 <P>Look at <code>.gitpod.yml</code> and  <code>.gitpod.Dockerfile</code> to see the gitpod-specific 
+                 parts for running postgres & staging data.
+                 <P>------------- end of countries.views.index"""
     return HttpResponse(
-        ("wireframe -- this is countries.views.index"
-         "<P>some data:</P>"
-         f"{the_facts}"))
+        (
+            "wireframe -- this is countries.views.index"
+            "<P>some data:</P>"
+            f"{the_facts}"
+            f"{so_what}"
+        )
+    )
+
